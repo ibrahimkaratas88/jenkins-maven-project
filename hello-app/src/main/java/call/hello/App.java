a7b406dc91119cbbf1b1319763d095e017eb6b8e
ibrahimkaratas88 <67653367+ibrahimkaratas88@users.noreply.github.com>
2020-11-08 21:27:07 +0300

    Update App.java

--- hello-app/src/main/java/call/hello/App.java
+++ hello-app/src/main/java/call/hello/App.java
@@ -3,7 +3,7 @@ package call.hello;
 public class App
 {
 
-    private final String message = "Hello World from Java Maven Project!";
+    private final String message = "Hello World from Java Maven Project_yeni_08 kasım!";
 
     public App() {}
 
